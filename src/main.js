fb27a35f684f2583f9f0f5e4ea92ea6f3d9aa15b
Greg <grtop@yandex.ru>
2023-05-02 16:19:22 +0300

    исправление в майн

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import FilterView from './view/filter-view';
 import InfoView from './view/trip-info';
 import ButtonView from './view/new-task-button-view';
 import WaypointsPresenter from './presenter/trip-presenter';
+import PointsModel from './model/model-waypoint.js';
 
 
 const tripFilters = document.querySelector('.trip-controls__filters');
 const tripMain = document.querySelector('.trip-main');
 const tripEvents = document.querySelector('.trip-events');
-const boardPresenter = new WaypointsPresenter({waypointsContainer: tripEvents});
+const pointsModel = new PointsModel;
+const boardPresenter = new WaypointsPresenter({waypointsContainer: tripEvents, pointsModel});
 
 render(new InfoView(), tripMain, RenderPosition.AFTERBEGIN);
 render(new FilterView(), tripFilters);
