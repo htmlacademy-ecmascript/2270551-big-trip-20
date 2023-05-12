2041b870661c1f87b431d75f6854bac3e05a4b53
Greg <grtop@yandex.ru>
2023-05-12 18:00:49 +0300

    использовал другой подход к случайному числу

--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,18 @@ const DATE_FORMAT_TIME = 'HH:mm';
 const DATE_FORMAT_MONTS = 'MMM D';
 const DATE_FORMAT_MONTS_TIME = 'DD/MM/YY hh:mm';
 
+/*function getRandomNumber(a, b) {
+
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}*/
+
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + min;
 
 const getRandomArrayElement = (items) => items[Math.floor(Math.random() * items.length)];
