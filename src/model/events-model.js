121cd043471ffb5f7f4a7ab9a90d0f8f2c36c537
Greg <grtop@yandex.ru>
2023-05-16 01:11:46 +0300

    описание кода модели событий

--- src/model/events-model.js
+++ src/model/events-model.js
@@ -1,11 +1,16 @@
+// модель событий
 import { getRandomEvent } from '../mocks/event.js';
 
-const EVENTS_NUMBER = 5;
+// количество предложений (оферов)
+const EVENTS_NUMBER = 7;
 
+// класс-модель массива предложений (оферов) количеством EVENTS_NUMBER
 export default class EventsModel {
   events = Array.from({ length: EVENTS_NUMBER }, getRandomEvent);
 
   getEvents() {
     return this.events;
   }
+
 }
+
