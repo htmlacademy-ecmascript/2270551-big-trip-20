2fe4e78ac47b5f47aa58c91421bf5bb02b3b5af0
Greg <grtop@yandex.ru>
2023-05-16 17:26:08 +0300

    ghf

--- src/model/destinations-model.js
+++ src/model/destinations-model.js
@@ -1,5 +1,6 @@
 import { createMockDestinations } from '../mocks/destinations.js';
 
+// класс-модель массива мест назначений количеством EVENTS_NUMBER
 export default class DestinationsModel {
   destinations = createMockDestinations();
 
